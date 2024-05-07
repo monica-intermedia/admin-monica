74a31fdce6789cae9c49117703cae4affa872339
Chandra Halim <chan.halim98@gmail.com>
2024-05-07 05:43:13 -0700

    f

--- pages/pegawai/jabatan/index.tsx
+++ pages/pegawai/jabatan/index.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import type { ReactElement } from "react";
+import SalaryTable from "../../../src/components/kas-keluar/SalaryTable";
+import PageContainer from "../../../src/components/container/PageContainer";
+import FullLayout from "../../../src/layouts/full/FullLayout";
+// import DashboardCard from "../../../src/components/shared/DashboardCard";
 
-const index = (): React.ReactElement => {
-  return <div>index</div>;
+const Index = (): any => {
+  return (
+    <PageContainer title="pembelian barang" description="this is Sample page">
+      <SalaryTable />
+    </PageContainer>
+  );
 };
 
-export default index;
+export default Index;
+Index.getLayout = function getLayout(page: ReactElement) {
+  return <FullLayout>{page}</FullLayout>;
+};
